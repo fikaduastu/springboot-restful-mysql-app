c1eaf7e5a53d9c6b1ffa7279bd04f4951c6e0da7
fikaduastu <1988.fikadufilipos@gmail.com>
2021-09-30 16:57:03 -0500

    application initialized

---
+++ src/main/java/com/fikadu/springbootrestfulmysqlapp/SpringbootRestfulMysqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.fikadu.springbootrestfulmysqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulMysqlAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestfulMysqlAppApplication.class, args);
+    }
+
+}
